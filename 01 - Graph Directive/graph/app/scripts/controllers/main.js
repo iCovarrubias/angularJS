7f12e4f92301822d08cddc7f38a2b1f64dd16061
iCovarrubias <pp_samael@hotmail.com>
2015-07-16 12:49:10 -0700

    Created a new project,  deleted the old one

---
+++ 01 - Graph Directive/graph/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name graphApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the graphApp
+ */
+angular.module('graphApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
